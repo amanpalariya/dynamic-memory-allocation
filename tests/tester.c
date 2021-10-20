49cdf8518ad72370bbb49c660a44148dfd2e68d7
Aman Palariya <2019csb1068@iitrpr.ac.in>
2021-10-20 09:16:07 +0530

    Write data structures test

--- tests/tester.c
+++ tests/tester.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include "../ds.h"
+#include "../logger.h"
 
 int total_tests = 0;
 int passed_tests = 0;
@@ -22,7 +23,71 @@ void print_test_section(char* section) {
     printf("\n\033[0;1m%s\033[0m\n\n", section);
 }
 
+void test_ds() {
+    {
+        struct partition* part = get_new_partition(NULL, NULL, 10, true);
+        test_log("New partition", part->is_free && part->size == 10);
+    }
+
+    struct memory* mem = get_new_empty_memory(100, 10);
+    test_log("New memory", mem->p == 100 && mem->q == 10 && mem->head->size == 90 && mem->head->is_free);
+
+    {
+        struct partition* part = allocate_partition(mem->head, mem->p);
+        test_log("Allocate on partition (1/2)", part == NULL);
+    }
+
+    {
+        int size = 50;
+        struct partition* part = allocate_partition(mem->head, size);
+        test_log("Allocate on partition (2/2)", part == mem->head && part->is_free == false && part->size == size && part->next->size == (mem->p - mem->q - size) && part->next->is_free);
+    }
+
+    {
+        deallocate_partition(mem->head);
+        test_log("Deallocate partition", mem->head->size == 90 && mem->head->next == NULL);
+    }
+
+    {
+        allocate_partition(mem->head, 5);
+        allocate_partition(mem->head->next, 10);
+        allocate_partition(mem->head->next->next, 6);
+        allocate_partition(mem->head->next->next->next, 8);
+        allocate_partition(mem->head->next->next->next->next, 4);
+        allocate_partition(mem->head->next->next->next->next->next, 20);
+        print_memory(mem);
+        deallocate_partition(mem->head->next);
+        deallocate_partition(mem->head->next->next->next);
+        deallocate_partition(mem->head->next->next->next->next->next);
+        print_memory(mem);
+
+        {
+            print_memory(mem);
+            first_fit(mem, 15);
+            struct partition* part = mem->head->next->next->next->next->next;
+            test_log("First fit", part->size == 15 && part->is_free == false);
+        }
+
+        {
+            struct partition* part = mem->head->next->next->next;
+            print_memory(mem);
+            best_fit(mem, 5);
+            test_log("Best fit", part->size == 5 && part->is_free == false);
+        }
+
+        {
+            struct partition* part = mem->head->next->next->next->next;
+            print_memory(mem);
+            next_fit(mem, 3, 16);
+            test_log("Next fit", part->size == 3 && part->is_free == false);
+        }
+    }
+}
+
 int main(int argc, char** argv) {
+    mute_logs();
+    print_test_section("Testing data structures");
+    test_ds();
     printf("\n%d/%d tests passed\n", passed_tests, total_tests);
     return 0;
 }
